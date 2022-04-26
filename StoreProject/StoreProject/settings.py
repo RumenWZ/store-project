5885789111474848dd65718e0278ca759bb0d119
RumenTashev <98177567+RumenWZ@users.noreply.github.com>
2022-04-27 00:43:47 +0300

    setting

--- StoreProject/StoreProject/settings.py
+++ StoreProject/StoreProject/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import cloudinary
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'StoreProject.main.urls',
 ]
 
 MIDDLEWARE = [
@@ -116,9 +119,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
+# AUTH_USER_MODEL = 'accounts.FitnessStoreUser'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#
+# cloudinary.config(
+#     cloud_name="dcjusaq1k",
+#     api_key="356371223124896",
+#     api_secret="tgaOZBfyLHt236EcNe_2n0rLHv8",
+# )
+#
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+#
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
