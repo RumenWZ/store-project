70941fc6e37d6901a71243b0f10a204223aa7be7
RumenTashev <98177567+RumenWZ@users.noreply.github.com>
2022-04-28 14:55:34 +0300

    templates

--- StoreProject/StoreProject/main/urls.py
+++ StoreProject/StoreProject/main/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = (
+from django.urls import path
 
+from StoreProject.main.views import HomeView, ItemDetailsView
+
+urlpatterns = (
+    path('', HomeView.as_view(), name='index'),
+    path('details/', ItemDetailsView.as_view(), name='details'),
 )
--- StoreProject/StoreProject/main/views.py
+++ StoreProject/StoreProject/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic as views
+
+
+class HomeView(views.TemplateView):
+    template_name = 'index.html'
+
+class ItemDetailsView(views.TemplateView):
+    template_name = 'detail.html'
