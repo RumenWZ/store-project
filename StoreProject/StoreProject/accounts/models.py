d7b1eb3f5137aa4ee41efc552abf6cba33bc2d96
RumenTashev <98177567+RumenWZ@users.noreply.github.com>
2022-04-30 18:17:17 +0300

    val

--- StoreProject/StoreProject/accounts/models.py
+++ StoreProject/StoreProject/accounts/models.py
@@ -1,4 +1,30 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
+from StoreProject.common.validators import validate_only_letters
+
+
 class Profile(models.Model):
+    FIRST_NAME_MIN_LEN = 2
+    FIRST_NAME_MAX_LEN = 20
+
+    LAST_NAME_MIN_LEN = 2
+    LAST_NAME_MAX_LEN = 20
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LEN,
+        validators=(
+            MinLengthValidator(FIRST_NAME_MIN_LEN),
+            validate_only_letters,
+        )
+    )
+
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LEN,
+        validators=(
+            MinLengthValidator(LAST_NAME_MIN_LEN),
+            validate_only_letters,
+        )
+    )
+
--- StoreProject/StoreProject/accounts/views.py
+++ StoreProject/StoreProject/accounts/views.py
@@ -1,10 +1,17 @@
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views import generic as views
+
+
 class LoginUserView(auth_views.LoginView):
     template_name = 'accounts/login.html'
 
 
-class RegisterUserView(auth_views.LoginView):
+class RegisterUserView(views.CreateView):
     template_name = 'accounts/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('index')
