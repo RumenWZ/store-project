7616535e80da02f8fc038f2bad66363357dca386
RumenTashev <98177567+RumenWZ@users.noreply.github.com>
2022-05-09 19:51:19 +0300

    fixed filtering bugs after applying paginator

--- StoreProject/StoreProject/main/views.py
+++ StoreProject/StoreProject/main/views.py
@@ -26,10 +26,27 @@ class CheckoutView(views.TemplateView):
 
 
 def shop_view(request):
-    ITEMS_PER_PAGE = 2
+    ITEMS_PER_PAGE = 6
+
+    # Filters
+    price_range_100 = request.GET.get('price-100')
+    price_range_200 = request.GET.get('price-200')
+    price_range_300 = request.GET.get('price-300')
+    product_contains = request.GET.get('product_contains')
 
     products = Product.objects.all()
     p = Paginator(Product.objects.all(), ITEMS_PER_PAGE)
+
+    if price_range_100:
+        p = Paginator(Product.objects.all().filter(actual_price__gt=0, actual_price__lt=101), ITEMS_PER_PAGE)
+    elif price_range_200:
+        p = Paginator(Product.objects.all().filter(actual_price__gt=100, actual_price__lt=201), ITEMS_PER_PAGE)
+    elif price_range_300:
+        p = Paginator(Product.objects.all().filter(actual_price__gt=200, actual_price__lt=301), ITEMS_PER_PAGE)
+
+    if product_contains != '' and product_contains is not None:
+        p = Paginator(Product.objects.all().filter(name__icontains=product_contains), ITEMS_PER_PAGE)
+
     page = request.GET.get('page')
     products_page = p.get_page(page)
 
@@ -42,30 +59,11 @@ def shop_view(request):
         product.actual_price = product.price * (100 - product.discount) / 100
         product.save()
 
-    price_range_100 = request.GET.get('price-100')
-    price_range_200 = request.GET.get('price-200')
-    price_range_300 = request.GET.get('price-300')
-
-    product_contains = request.GET.get('product_contains')
-
-    if product_contains != '' and product_contains is not None:
-        products = products.filter(name__icontains=product_contains)
-
     products_all_count = products.count()
     products_in_range_0_100 = products.filter(actual_price__gt=0, actual_price__lt=101).count()
     products_in_range_100_200 = products.filter(actual_price__gt=100, actual_price__lt=201).count()
     products_in_range_200_300 = products.filter(actual_price__gt=200, actual_price__lt=301).count()
 
-    print(max_page_num)
-
-    if price_range_100:
-        products = products.filter(actual_price__gt=0, actual_price__lt=101)
-    elif price_range_200:
-        products = products.filter(actual_price__gt=100, actual_price__lt=201)
-    elif price_range_300:
-        products = products.filter(actual_price__gt=200, actual_price__lt=301)
-
-    print(f'prev twice: {previous_page_twice}')
     context = {
         'products': products,
         'products_all_count': products_all_count,
@@ -79,7 +77,6 @@ def shop_view(request):
     }
 
     return render(request, 'shop.html', context)
-
 
 
 class ContactView(views.TemplateView):
